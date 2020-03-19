dda81a661885266d5338c73eefb3a849989a29dc
Mayank Dhanawade <mayank.dhanawade@gmail.com>
2020-03-19 17:52:50 +0530

    #4 added interface for database

---
+++ lib/helpers/Database.dart
@@ -0,0 +1,15 @@
+class Database {
+  
+  ///Function to save data
+  static void saveData() {
+  }
+
+  ///Function to upload image to fireBase
+  static void uploadImage() {
+  }
+
+  /// Function to get data
+  static void getData() {
+
+  }
+}
