f61184f6d40aa70131b9942562e16ef3d9808ef7
Mayank Dhanawade <mayank.dhanawade@gmail.com>
2020-03-23 16:27:45 +0530

    #4 updated comment

--- form_builder_sample/lib/main.dart
+++ form_builder_sample/lib/main.dart
@@ -18,7 +18,7 @@ void main() async {
   runApp(MyApp());
 }
 
-///MyApp entry of Finday app
+///MyApp entry of form builder sample
 class MyApp extends StatelessWidget {
   /// Static variable to refer this class in routes
   static const id = '/';
