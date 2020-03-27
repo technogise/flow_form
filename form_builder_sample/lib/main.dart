a0f35ab4cbcd598fadad600680ce00581db34cd7
Burhanuddin Rashid <burhanrashid5253@gmail.com>
2020-03-27 15:33:11 +0530

    #32 Added builder pattern to setup form

--- form_builder_sample/lib/main.dart
+++ form_builder_sample/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:formbuilder/form.dart';
 import 'package:formbuilder/formbuilder.dart';
-import 'package:formbuilder/redux/actions/actions.dart';
-import 'package:formbuilder/redux/store.dart';
 import 'package:formbuildersample/database_impl.dart';
 import 'package:formbuildersample/widgets/screens/dashboard.dart';
 import 'package:formbuildersample/widgets/screens/date_picker_screen.dart';
@@ -9,7 +8,6 @@ import 'package:formbuildersample/widgets/screens/file_upload_screen.dart';
 import 'package:formbuildersample/widgets/screens/section_screen.dart';
 import 'package:formbuildersample/widgets/screens/select_screen.dart';
 import 'package:formbuildersample/widgets/screens/text_input_screen.dart';
-import 'package:formbuilder/widget_registry.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart' as path_provider;
 
@@ -18,13 +16,21 @@ import 'widgets/screens/main_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  registerWidgets();
   initHiveDB();
-  var flowPath = "assets/json/flow.json";
-  var dataPath = "assets/json/screen_data.json";
-  setFlowAndDataPath(flowPath, dataPath);
-  setupDB(DatabaseImpl());
-  runApp(MyApp());
+
+  final flowFormBuilder = FlowFormBuilder("assets/json/flow.json")
+    ..screenMetaPath = "assets/json/screen_data.json"
+    ..database = DatabaseImpl()
+    ..registerWidgets = {
+      SelectScreen.type: SelectScreen(),
+      DatePickerScreen.type: DatePickerScreen(),
+      Dashboard.type: Dashboard(),
+      TextInputScreen.type: TextInputScreen(),
+      RenderSection.type: RenderSection(),
+      FileUploadScreen.type: FileUploadScreen()
+    };
+
+  runApp(MyApp(flowFormBuilder.build()));
 }
 
 ///MyApp entry of form builder sample
@@ -32,9 +38,14 @@ class MyApp extends StatelessWidget {
   /// Static variable to refer this class in routes
   static const id = '/';
 
+  final FlowForm flowForm;
+
+  MyApp(this.flowForm);
+
   @override
   Widget build(BuildContext context) {
     return FormBuilderProvider(
+      flowForm: flowForm,
       child: MaterialApp(
         theme: appTheme,
         home: Scaffold(
@@ -57,18 +68,4 @@ void initHiveDB() async {
       await path_provider.getApplicationDocumentsDirectory();
   Hive.init(appDocumentDirectory.path);
   await Hive.openBox("DataBox");
-}
-
-void registerWidgets() {
-  var map = {
-    SelectScreen.type: SelectScreen(),
-    DatePickerScreen.type: DatePickerScreen(),
-    Dashboard.type: Dashboard(),
-    TextInputScreen.type: TextInputScreen(),
-    RenderSection.type: RenderSection(),
-    FileUploadScreen.type: FileUploadScreen()
-  };
-
-  final widgetRegistry = getRegistry();
-  widgetRegistry.registerWidget(map);
 }
